576984b7d3dc68aa85a4d19c14c3d65a6bcd0d9e
Pavel <pavelgq@yandex.ru>
2022-06-29 19:45:26 +0300

    fix: images paths

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -5,47 +5,47 @@ export const data: SomeDataI[] = [
   {
     title: "SOFA",
     category: "Design",
-    imageUrl: "/assets/image/sofa-1x.png",
+    imageUrl: "./assets/image/sofa-1x.png",
   },
   {
     title: "Key Board",
     category: "Branding",
-    imageUrl: "/assets/image/keyboard-1x.png",
+    imageUrl: "./assets/image/keyboard-1x.png",
   },
   {
     title: "Work Media",
     category: "Illustration",
-    imageUrl: "/assets/image/work-media-1x.png",
+    imageUrl: "./assets/image/work-media-1x.png",
   },
   {
     title: "DDDone",
     category: "Motion",
-    imageUrl: "/assets/image/dddone-1x.png",
+    imageUrl: "./assets/image/dddone-1x.png",
   },
   {
     title: "Abstract",
     category: "Design",
-    imageUrl: "/assets/image/abstract-1x.png",
+    imageUrl: "./assets/image/abstract-1x.png",
   },
   {
     title: "HangP",
     category: "Branding",
-    imageUrl: "/assets/image/hand-p-1x.png",
+    imageUrl: "./assets/image/hand-p-1x.png",
   },
   {
     title: "Architect",
     category: "Motion",
-    imageUrl: "/assets/image/architect-1x.png",
+    imageUrl: "./assets/image/architect-1x.png",
   },
   {
     title: "CalC",
     category: "Design",
-    imageUrl: "/assets/image/calc-1x.png",
+    imageUrl: "./assets/image/calc-1x.png",
   },
   {
     title: "Sport",
     category: "Branding",
-    imageUrl: "/assets/image/sport-1x.png",
+    imageUrl: "./assets/image/sport-1x.png",
   },
 ];
 
